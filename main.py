f34a17ee15b4ee4fc97aa906fd16e02e4d8718a2
AnujTimsina <58809699+AnujTimsina@users.noreply.github.com>
2020-03-23 10:24:35 +0545

    Add files via upload
    icon named bfphoto_icon.ico has been added and photo is added but doesnt work as expected

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ root = Tk()
 root.title("BRITISH ARMY TRAINING CENTER")
 root.configure(background="#b3c1f2")
 root.option_add( "*font", "Verdana  18" )  #setting default font and fontsize
-
+root.iconbitmap("bgphoto_icon.ico")
 #making frame
 insert_frame= LabelFrame(root, padx=200,pady=50, bg="#8290bf")
 view_frame= LabelFrame(root, bg="#455959")
@@ -17,43 +17,43 @@ view_frame= LabelFrame(root, bg="#455959")
 insert_frame.grid( row=2, column= 0 ,padx=5, pady=5)
 view_frame.grid( row=0, column= 0,padx=5, pady=5)
 
-'''#making fullscreen
-width_value= root.winfo_screenwidth()
-height_value=root.winfo_screenwidth()
-root.geometry("%dx%d" %(width_value,height_value))'''
-
+#adding image
+image_frame= LabelFrame(view_frame)
+image_frame.grid( row=0, column= 0)
+bgimage=PhotoImage(file= r"bgphoto.png")
+Label(image_frame,image= bgimage).place(relwidth=1,relheight=1)
 
 def add():
-    df = pd.DataFrame({'Name': [name_data.get()],
-                    "Father's Name":[fathers_name_data.get()],
-                    "GrandFather's Name":[grandfathers_name_data.get()],
-                    'Age': [age_data.get()],
-                    'Address':[address_data.get()],
-                    'Contact':[contact_data.get()]
-
-                })
-    writer = pd.ExcelWriter('demo.xlsx', engine='openpyxl')
+    if (not name_data.get) or (not age_data.get()) or (not address_data.get()) or (not contact_data.get()) or (not amount_data.get()):
+	    pop("Empty Field","All field are necessary")
+    else:
+        df = pd.DataFrame({'Name': [name_data.get()],
+    		"Father's Name":[fathers_name_data.get()],
+    		"GrandFather's Name":[grandfathers_name_data.get()],
+    		'Age': [age_data.get()],
+    		'Address':[address_data.get()],
+    		'Contact':[contact_data.get()],
+    		'Amount Paid':[amount_data.get()],
+            'Month':[amount_data.get()]
+    		})
+        writer = pd.ExcelWriter('demo.xlsx', engine='openpyxl')
 # try to open an existing workbook
-    writer.book = load_workbook('demo.xlsx')
+        writer.book = load_workbook('demo.xlsx')
 # copy existing sheets
-    writer.sheets = dict((ws.title, ws) for ws in writer.book.worksheets)
+        writer.sheets = dict((ws.title, ws) for ws in writer.book.worksheets)
 # read existing file
-    reader = pd.read_excel(r'demo.xlsx')
+        reader = pd.read_excel(r'demo.xlsx')
 # write out the new sheet
-    df.to_excel(writer,index=False,header=False,startrow=len(reader)+1)
-
-
-    writer.close()
-    pop("Data enetred","Entered value sucessfully")
+        df.to_excel(writer,index=False,header=False,startrow=len(reader)+1)
+        writer.close()
+        pop("Data enetred","Entered value sucessfully")
 def pop(data1,data2):
      messagebox.showinfo(data1,data2)
 #writer.close()
 def delete(val):
     df = pd.read_excel('demo.xlsx', index_col=[0])
     df = df.drop([val], axis=0)
-    
     df.to_excel("demo.xlsx")
-
     #print("Deleted sucessfully")
 def search():
     try:
@@ -65,26 +65,23 @@ def search():
         d=0   
         for row in ws.rows:
             c = 1
-
             if (row[0].value == "Name"):
             
                 for cell in row:
                     Label(top,text=cell.value).grid(row=r,column=c)
                     c+=1
                 r+=1
-            if row[0].value == search_data.get():
+            if row[0].value.lower() == search_data.get().lower():
                 for cell in row:
                     Label(top,text=cell.value).grid(row=r,column=c)
                     c+=1 
                     d=1              
                 r+=1
-        print (d)
         if d==0:
             Label(top,text="Sorry No data available for that name.").grid(row=r,column=c)
     except:
         pop("Unable to find","Unable to retrieve data.")
 def open(value):
-
     top=Toplevel()
     top.title("View recruits info")
     Label(top,text=value).pack()
@@ -98,11 +95,13 @@ def edit(name):
 def view():
     top=Toplevel()
     top.title("View recruits info")
+    #making scrollbar-vertical
+    scrollbar = Scrollbar(top)
+    scrollbar.grid(column=4)
     df=pd.read_excel('demo.xlsx', index_col=0)
     file = "demo.xlsx"
     wb = load_workbook(file, data_only=True)
     ws = wb.active
-
     r= 1
     for row in ws:
         c = 1
@@ -111,16 +110,8 @@ def view():
             c+=1
         r+=1
 
-
-#function for view button
-'''def open():
-    top=Toplevel()
-    top.title("View recruits info")'''
-
-
-'''def viewrec():
-    df=pd.read_excel('demo.xlsx', index_col=0)
-    tkMessageBox.showinfo("Details", str(df))'''
+def update():
+    pass
 
 name_data=StringVar()
 fathers_name_data=StringVar()
@@ -129,17 +120,22 @@ age_data=StringVar()
 address_data=StringVar()
 contact_data=StringVar()
 search_data=StringVar()
+amount_data=StringVar()
+month = StringVar()
+#root.resizable(height=false,width =false)
 #buttons
 insert = Button(insert_frame, text="INSERT NEW RECRUITS",bg="#e4eded", fg="#063332",width=25, command=add)
 view = Button(view_frame, text="VIEW RECRUITS INFO", bg="#e4eded", fg="#063332",command=view)
+update = Button(view_frame, text="UPDATE RECRUITS INFO", bg="#e4eded", fg="#063332",command=update)
 search = Button(view_frame, text="SEARCH", bg="#e4eded", fg="#063332",command=search)
 #only for search
 search_e = Entry(view_frame, textvariable=search_data)
-search_e.grid(row=0, column=4)
+search_e.grid(row=0, column=5)
 
-insert.grid( row=7, column= 2)
-view.grid( row=0, column= 1)
-search.grid( row=0, column= 2)
+insert.grid( row=8, column= 2)
+view.grid( row=0, column= 2)
+update.grid( row=0, column= 3)
+search.grid( row=0, column= 4)
 
 
 #insert labels 
@@ -150,6 +146,7 @@ grandfathers_name = Label(insert_frame, text="Grandfather's name :", padx=20, pa
 age = Label(insert_frame, text="Age :", padx=20, pady=20, bg="#8290bf",fg="white")
 address = Label(insert_frame, text="Address :", padx=20, pady=20, bg="#8290bf",fg="white")
 contact = Label(insert_frame, text="Contact :", padx=20, pady=20, bg="#8290bf",fg="white")
+amount = Label(insert_frame, text="Amount Paid :", padx=20, pady=20, bg="#8290bf",fg="white")
 
 #entry
 name_e = Entry(insert_frame, textvariable=name_data, width=25)
@@ -158,6 +155,7 @@ grandfathers_name_e = Entry(insert_frame, textvariable=grandfathers_name_data,wi
 age_e = Entry(insert_frame, textvariable=age_data,width=25)
 address_e = Entry(insert_frame, textvariable=address_data,width=25)
 contact_e = Entry(insert_frame, textvariable=contact_data,width=25)
+amount_e = Entry(insert_frame, textvariable=amount_data,width=12)
 
 
 name.grid(row=1, column=0)
@@ -172,7 +170,13 @@ address.grid(row=5, column=0)
 address_e.grid(row=5, column=2, sticky="ew")
 contact.grid(row=6, column=0)
 contact_e.grid(row=6, column=2, sticky="ew")
-
+amount.grid(row=7, column=0)
+amount_e.grid(row=7, column=2 , sticky = "w")
+list1= ['Baisakh','Jestha','Ashad','Shrawan','Bhadra','Ashwin','Kartik','Mangsir','Poush','Magh','Flagun','Chaitra']
+droplist = OptionMenu(insert_frame,month,*list1)
+droplist.config(width = 10)
+month.set('Baisakh')
+droplist.grid(row = 7,column =2, sticky= "e")
 
 
 
